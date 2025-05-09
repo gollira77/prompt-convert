fb75ba54f202f5025929da87169bfc95bd435aba
Villalba Franco <franquivillalba13@gmail.com>
2025-05-09 12:22:51 -0300

    definicion de variables

---
+++ app.js
@@ -0,0 +1,6 @@
+// creación de la variable de la tasa del cambio que sera constanten siempre 
+// y tambien se creo el arreglo donde se guardan las operaciones del cliente
+const tasaCambio = 1300;
+let historial = [];
+
+
